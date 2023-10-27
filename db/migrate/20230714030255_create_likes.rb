a36d79cb9ac0b6fa5b0f7a8cb54143fe46e695bf
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-27 13:24:33 +0100

    make changes

--- db/migrate/20230714030255_create_likes.rb
+++ db/migrate/20230714030255_create_likes.rb
@@ -1,9 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
     def change
       create_table :likes do |t|
-        t.references :authors, null: false, foreign_key: true
-        t.references :posts, null: false, foreign_key: true
-  
+        t.references :user, foreign_key: true
+        t.references :post, foreign_key: true
         t.timestamps
       end
     end
--- db/migrate/20230714030342_create_comments.rb
+++ db/migrate/20230714030342_create_comments.rb
@@ -1,11 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
-    def change
-      create_table :comments do |t|
-        t.text :text
-        t.references :authors, null: false, foreign_key: true
-        t.references :posts, null: false, foreign_key: true
-  
-        t.timestamps
-      end
+  def change
+    create_table :comments do |t|
+      t.string :text
+
+      t.timestamps
     end
   end
+end
