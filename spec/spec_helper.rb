09281f4bf6673cdb9ecf055cf91e604676f2e120
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-11-03 14:02:07 +0100

    fix linters

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,8 +53,8 @@ RSpec.configure do |config|
   #   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   #   config.filter_run_when_matching :focus
   #
-  #   # the `--only-failures` and `--nex  # Allows RSpec to persist some state between runs in order to support
-  # t-failure` CLI options. We recommend
+  #   # Allows RSpec to persist some state between runs in order to support
+  #   # the `--only-failures` and `--next-failure` CLI options. We recommend
   #   # you configure your source control system to ignore this file.
   #   config.example_status_persistence_file_path = "spec/examples.txt"
   #
@@ -62,10 +62,6 @@ RSpec.configure do |config|
   #   # recommended. For more details, see:
   #   # https://rspec.info/features/3-12/rspec-core/configuration/zero-monkey-patching-mode/
   #   config.disable_monkey_patching!
-  #
-  #   # This setting enables warnings. It's recommended, but in some cases may
-  #   # be too noisy due to issues in dependencies.
-  #   config.warnings = true
   #
   #   # Many RSpec users commonly either run the entire suite or an individual
   #   # file, and it's useful to allow more verbose output when running an
